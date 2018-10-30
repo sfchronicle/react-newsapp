88939a44c3716bcd77c3290c1ef4b6b20d186d10
ewagstaff <evanjwagstaff@gmail.com>
2018-10-30 10:24:34 -0700

    Made creditline flexible if props are missing

--- root/components/CreditLine.js
+++ root/components/CreditLine.js
@@ -4,10 +4,15 @@ import React, { Component } from "react";
 class CreditLine extends Component {
   render() {
     return (
-      <p>{this.props.name} • <a href={`mailto:${ this.props.email } `}>{ this.props.email }</a> • <a target="_blank" rel="noopener" href={`https://twitter.com/${ this.props.twitter } `}>@{ this.props.twitter }</a></p>
+      <p>{this.props.name}
+      {this.props.email &&
+      	<span>&nbsp;• <a href={`mailto:${ this.props.email } `}>{ this.props.email }</a> </span>
+      }
+      {this.props.twitter &&
+      	<span>&nbsp;• <a target="_blank" rel="noopener" href={`https://twitter.com/${ this.props.twitter } `}>@{ this.props.twitter }</a></span>
+      }
+      </p>
     );
   }
 }
 export default CreditLine;
-
-		
